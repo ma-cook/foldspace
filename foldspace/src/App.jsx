e6c32600371209cea3ee8086e3d925385255f6e7
Mitchell <mitchell.cook@outlook.co.nz>
2024-10-23 01:38:24 +1300

    testing deployment

--- foldspace/src/App.jsx
+++ foldspace/src/App.jsx
@@ -25,7 +25,6 @@ import LoadingMessage from './LoadingMessage'; // Import LoadingMessage
 import loadCell from './loadCell';
 import unloadCell from './unloadCell';
 
-//testing deployment, aligning keys//
 const App = React.memo(() => {
   const loadedCells = useStore((state) => state.loadedCells);
   const positions = useStore((state) => state.positions);
