70e9065d172b866b9db46083e26dfd98be46c3e6
Mitchell <mitchell.cook@outlook.co.nz>
2024-12-02 05:09:07 +1300

    optimise planet name rendering

--- foldspace/src/Sphere.jsx
+++ foldspace/src/Sphere.jsx
@@ -1,5 +1,5 @@
 // Sphere.jsx
-import React, { useEffect, useRef, forwardRef } from 'react';
+import React, { useMemo, useRef, forwardRef } from 'react';
 import * as THREE from 'three';
 import { Html } from '@react-three/drei';
 
@@ -17,19 +17,43 @@ const Sphere = forwardRef(
   ) => {
     const meshRef = useRef();
 
-    useEffect(() => {
+    const customInstancePositions = useMemo(() => {
+      const positionsArray = new Float32Array(positions.length * 3);
+      positions.forEach((position, index) => {
+        positionsArray.set(
+          position instanceof THREE.Vector3
+            ? [position.x, position.y, position.z]
+            : [position[0], position[1], position[2]],
+          index * 3
+        );
+      });
+      return positionsArray;
+    }, [positions]);
+
+    const instanceMatrices = useMemo(() => {
+      const matrices = new Float32Array(positions.length * 16);
+      const quaternion = new THREE.Quaternion().setFromEuler(
+        new THREE.Euler(...rotation)
+      );
+      const scaleVector = new THREE.Vector3(...scale);
+      positions.forEach((position, index) => {
+        const posVector =
+          position instanceof THREE.Vector3
+            ? position
+            : new THREE.Vector3(...position);
+        const matrix = new THREE.Matrix4().compose(
+          posVector,
+          quaternion,
+          scaleVector
+        );
+        matrix.toArray(matrices, index * 16);
+      });
+      return matrices;
+    }, [positions, scale, rotation]);
+
+    useMemo(() => {
       const mesh = meshRef.current;
       if (mesh) {
-        const customInstancePositions = new Float32Array(positions.length * 3);
-        positions.forEach((position, index) => {
-          customInstancePositions.set(
-            position instanceof THREE.Vector3
-              ? [position.x, position.y, position.z]
-              : [position[0], position[1], position[2]],
-            index * 3
-          );
-        });
-
         mesh.geometry.setAttribute(
           'customInstancePosition',
           new THREE.InstancedBufferAttribute(customInstancePositions, 3)
@@ -39,23 +63,33 @@ const Sphere = forwardRef(
           material.customInstancePositions = customInstancePositions;
         }
 
-        const quaternion = new THREE.Quaternion().setFromEuler(
-          new THREE.Euler(...rotation)
-        );
-
-        positions.forEach((position, index) => {
-          const matrix = new THREE.Matrix4().compose(
-            position instanceof THREE.Vector3
-              ? position
-              : new THREE.Vector3(...position),
-            quaternion,
-            new THREE.Vector3(...scale)
+        for (let i = 0; i < positions.length; i++) {
+          const matrix = new THREE.Matrix4().fromArray(
+            instanceMatrices,
+            i * 16
           );
-          mesh.setMatrixAt(index, matrix);
-        });
+          mesh.setMatrixAt(i, matrix);
+        }
         mesh.instanceMatrix.needsUpdate = true;
       }
-    }, [positions, scale, rotation, material]);
+    }, [customInstancePositions, instanceMatrices, material, positions.length]);
+
+    // Optimize planet names rendering
+    const planetNameElements = useMemo(() => {
+      return Object.keys(planetNames).map((key) => {
+        const position = key.split(',').map(Number);
+        const planetName = planetNames[key];
+        return (
+          <Html
+            key={key}
+            position={[position[0], position[1] + 1, position[2]]}
+            center
+          >
+            <div className="planet-name">{planetName}</div>
+          </Html>
+        );
+      });
+    }, [planetNames]);
 
     return (
       <>
@@ -68,36 +102,12 @@ const Sphere = forwardRef(
               ref.current = node;
             }
           }}
-          args={[geometry, null, positions.length]} // Set material to null initially
+          args={[geometry, null, positions.length]}
           frustumCulled={false}
         >
           <primitive attach="material" object={material} />
         </instancedMesh>
-        {positions.map((position, index) => {
-          // Construct the key from position coordinates
-          const key =
-            position instanceof THREE.Vector3
-              ? `${position.x},${position.y},${position.z}`
-              : `${position[0]},${position[1]},${position[2]}`;
-
-          const planetName = planetNames[key];
-          if (planetName) {
-            return (
-              <Html
-                key={key}
-                position={
-                  position instanceof THREE.Vector3
-                    ? [position.x, position.y + 1, position.z]
-                    : [position[0], position[1] + 1, position[2]]
-                }
-                center
-              >
-                <div className="planet-name">{planetName}</div>
-              </Html>
-            );
-          }
-          return null;
-        })}
+        {planetNameElements}
       </>
     );
   }
