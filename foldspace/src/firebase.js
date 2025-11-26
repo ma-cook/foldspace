b44dc12307cb3bd9f8e1ebf9018fc03a37b9d7e7
Mitchell <mitchell.cook@outlook.co.nz>
2025-11-26 20:48:53 +1300

    updated key info

--- foldspace/src/firebase.js
+++ foldspace/src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBy6tXwJz488MzngGStNPLr2U8t0J7utCk',
-  authDomain: 'foldspace-6483c.firebaseapp.com',
-  projectId: 'foldspace-6483c',
-  storageBucket: 'foldspace-6483c.appspot.com',
-  messagingSenderId: '1049446072988',
-  appId: '1:1049446072988:web:d679e61a7066eb29e22359',
-  measurementId: 'G-MPMLJQEN1F',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
